e540ad80be7fc2802f670187c8f9a8eb45176334
Ignacio Gonzalez-Bueno <igbescobar@gmail.com>
2024-04-18 11:27:21 +0200

    Creación del modelo Usuario

--- loteriaPrimitiva/src/main/java/com/babel/loteriaPrimitiva/Modelo/Usuario.java
+++ loteriaPrimitiva/src/main/java/com/babel/loteriaPrimitiva/Modelo/Usuario.java
@@ -1,4 +1,30 @@
 package com.babel.loteriaPrimitiva.Modelo;
 
+import java.util.List;
+import java.util.Set;
+
 public class Usuario {
+	private int id;
+	private Set<Apuesta> apuesta;
+
+	public Usuario(int id, List<Apuesta> apuesta) {
+		this.id = id;
+		this.apuesta = (Set<Apuesta>) apuesta;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Set<Apuesta> getApuesta() {
+		return this.apuesta;
+	}
+
+	public void setApuesta(Set<Apuesta> apuesta){
+		this.apuesta = apuesta;
+	}
 }
