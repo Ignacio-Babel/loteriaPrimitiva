5461049b1de34a61f6b1e814d4553975d09500a1
Ignacio Gonzalez-Bueno <igbescobar@gmail.com>
2024-04-18 10:50:42 +0200

    creacion de proyecto

---
+++ loteriaPrimitiva/src/main/java/com/babel/loteriaPrimitiva/LoteriaPrimitivaApplication.java
@@ -0,0 +1,13 @@
+package com.babel.loteriaPrimitiva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoteriaPrimitivaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoteriaPrimitivaApplication.class, args);
+	}
+
+}
---
+++ loteriaPrimitiva/src/test/java/com/babel/loteriaPrimitiva/LoteriaPrimitivaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.babel.loteriaPrimitiva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoteriaPrimitivaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
