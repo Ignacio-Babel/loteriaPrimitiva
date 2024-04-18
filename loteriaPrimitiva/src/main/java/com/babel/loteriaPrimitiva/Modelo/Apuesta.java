a4ca3925581ca8f65cad60f26165246032f1a3a0
andres.montaño <andres.montano@babelgroup.com>
2024-04-18 12:00:38 +0200

    -Implementacion objeto apuesta

--- loteriaPrimitiva/src/main/java/com/babel/loteriaPrimitiva/Modelo/Apuesta.java
+++ loteriaPrimitiva/src/main/java/com/babel/loteriaPrimitiva/Modelo/Apuesta.java
@@ -1,4 +1,25 @@
 package com.babel.loteriaPrimitiva.Modelo;
 
+import java.util.Objects;
+import java.util.Set;
+
 public class Apuesta {
+
+    private Set<Integer> numerosApuesta;
+    public Apuesta(Set<Integer> numerosApuesta) {
+        this.numerosApuesta = numerosApuesta;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Apuesta apuesta = (Apuesta) o;
+        return Objects.equals(numerosApuesta, apuesta.numerosApuesta);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(numerosApuesta);
+    }
 }
